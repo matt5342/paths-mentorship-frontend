7af273d00eae37fa98d6660c3f9346fc078c0329
matt5342 <mattfsewell@gmail.com>
2021-06-14 20:12:41 -0400

    login working

--- src/App.js
+++ src/App.js
@@ -1,5 +1,9 @@
 import './App.css';
-import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import React, { useEffect } from 'react';
+import { Router, Route, Switch, Redirect } from 'react-router-dom';
+import { useDispatch, useSelector } from 'react-redux';
+import { history } from './_helpers/history';
+import { alertActions } from './_actions/alert.actions';
 import Navigation from './components/Navigation'
 import Home from './components/Home'
 import Program from './components/Program'
@@ -15,16 +19,29 @@ import Impact from './components/Impact';
 
 function App() {
 
+  const alert = useSelector(state => state.alert);
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+      history.listen((location, action) => {
+          // clear alert on location change
+          dispatch(alertActions.clear());
+      });
+  }, []);
+
   return (
     <div className="wrapper">
       <Navigation />
       <div className="container">
+        {alert.message &&
+          <div className={`alert ${alert.type}`}>{alert.message}</div>
+        }
         
 
       </div>
       
       <br></br><br></br>
-      <BrowserRouter>
+      <Router history={history}>
         <Switch>
           <Route exact path="/home" component={Home} />
           <Route exact path="/" component={Home} />
@@ -39,7 +56,7 @@ function App() {
           
         </Switch>
 
-      </BrowserRouter>
+      </Router>
       <NavBottom/>
     </div>
   );
--- src/index.js
+++ src/index.js
@@ -4,10 +4,14 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import './custom.scss';
+import { Provider } from 'react-redux';
+import { store } from './_helpers/store';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </React.StrictMode>,
   document.getElementById('root')
 );
