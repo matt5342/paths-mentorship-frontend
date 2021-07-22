9a850c7c157e45e68d851fe2c88a2b6484539160
matt5342 <mattfsewell@gmail.com>
2021-07-22 17:16:06 -0400

    fixed program table scrollspy links

--- src/components/presentational/ProgramTable.js
+++ src/components/presentational/ProgramTable.js
@@ -17,20 +17,20 @@ const ProgramTable = () =>
                 </td>
                 </tr>
                 <tr>
-                <td><strong><a href="#dev-workshops">Development Workshops</a></strong></td>
+                <td><strong><a href="/programs#dev-workshops">Development Workshops</a></strong></td>
                 <td>
                     Helps students develop the skills required to succeed in college and in academic/professional environments
                 </td>
                 </tr>
                 <tr>
-                <td ><strong><a href="#action-potential-initiative">Action Potential Initiative</a></strong></td>
+                <td ><strong><a href="/programs#action-potential-initiative">Action Potential Initiative</a></strong></td>
                 <td>
                     As part of the Action Potential Initiative, students optionally develop a scientific research question and carry out a small research project. 
                     They are led through the scientific method and guided through the project by their mentor. Students must apply to participate in this part of the program.
                 </td>
                 </tr>
                 <tr>
-                <td ><strong><a href="#career-seminars">Career Seminars</a></strong></td>
+                <td ><strong><a href="/programs#career-seminars">Career Seminars</a></strong></td>
                 <td>
                     Working professionals present to students about their careers, sharing the day to day life and steps necessary 
                     to reach their career. Emphasis on non-typical health-related careers.
