c820aff7a5c882560cc06a24365829b452d660ec
matt5342 <mattfsewell@gmail.com>
2021-08-19 17:57:36 -0400

    prod url

--- src/_helpers/base-url.js
+++ src/_helpers/base-url.js
@@ -1,2 +1,2 @@
-export const BASE_URL = 'http://localhost:8080'
-// export const BASE_URL = 'https://paths-mentorship.herokuapp.com'
+// export const BASE_URL = 'http://localhost:8080'
+export const BASE_URL = 'https://paths-mentorship.herokuapp.com'
